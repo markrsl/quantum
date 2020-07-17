23a1bbbbc49d9c83f68906b12d8ac1c1cb865ac3
Mark <s108321505@mail1.ncnu.edu.tw>
2020-07-17 15:45:58 +0800

    updated calculating method

--- main.cpp
+++ main.cpp
@@ -1,72 +1,50 @@
-#include <iostream>
 #include <cmath>
-#include <vector>
-#include <iomanip>
+#include <string>
 #include "Logger.h"
 
 #define PI 3.1415926535897932384626433832795
 
 using namespace std;
 
-int nWire = 1;
-int inverseBit = 1;
-int nRuns = 1;
+long long numItem = 2; // the number of total items.
+int numSelectedItem = 1; // the number of selected items.
+int numSearch = 1; // the number of search, i.e., iteration count.
+double ampI; // the amplitude of selected items.
+double ampO; // the amplitude of unselected items.
 
-bool isMatch(vector<double> &amp) {
-    for (int i = amp.size() - 1; i >= nWire - inverseBit; i--) {
-//        if (abs(amp[i]) > 0)
-        if (abs(amp[i]) > 0.0000000000000001)
-            return false;
-    }
-    return true;
-}
-
-void prtSum(vector<double> &amp) {
-    double sum = 0.0;
-    for (int i = 0; i < inverseBit; i++) {
-        sum += amp[i] * amp[i];
-    }
-    cout << sum * 100.0 << endl;
-}
-
-void run(vector<double> &amp, int count) {
-    if (count == nRuns)
-        return;
-    else {
-        // selective inversion
-        for (int i = 0; i < inverseBit; i++)
-            amp[i] = -amp[i];
+void run() {
+    for (int i = 0; i < numSearch; i++) {
+        ampI = -ampI; // selective inversion
 
         // inversion about average
-        double avg = 0.0;
-        for (auto it:amp)
-            avg += it;
-        avg /= nWire;
-        for (auto &it :amp)
-            it = 2.0 * avg - it;
-        if (isMatch(amp)) {
-            Logger logger("../match.csv");
-            logger.writeComma(nWire);
-            logger.writeLine(count + 1);
-            cout << nWire << "," << count + 1 << endl;
-        }
-        run(amp, count + 1);
+        double avg = (ampI * numSelectedItem + ampO * (numItem - numSelectedItem)) / numItem;
+        ampI = 2.0 * avg - ampI;
+        ampO = 2.0 * avg - ampO;
+
+        Logger logger("../result" + to_string(int(numItem / 10000000)) + ".csv");
+        logger.writeComma(numItem);
+        logger.writeComma(ampI);
+        logger.writeComma(ampO);
+        logger.writeComma(i + 1);
+        if (abs(ampO * (numItem - numSelectedItem)) <= 0.0000000000000001)
+            logger.writeLine(1); // found
+        else
+            logger.writeLine(0); // not found
     }
 }
 
 int main() {
-    cout << fixed << setprecision(10);
-    cout << "input nWire and maxWire" << endl;
-    int maxWire;
-    cin >> nWire >> maxWire;
-    if (nWire < inverseBit)
-        return -9999;
-    while (nWire <= maxWire) {
-        vector<double> amp(nWire, 1.0 / sqrt(nWire));
-//        cout << nWire << ',';
-        nRuns = ceil((PI / 2.0 - 1.0 / sqrt(nWire)) / (2.0 / sqrt(nWire)));
-        run(amp, 0);
-        nWire++;
+    long long max = pow(2, 31);
+
+    while (numItem < max) {
+        ampI = 1.0 / sqrt(numItem);
+        ampO = 1.0 / sqrt(numItem);
+        double deg = asin(1.0 / sqrt(numItem)) * 180.0 / PI;
+        numSearch = ceil((90.0 - deg) / (2.0 * deg));
+        if (numItem % 100000 == 0)
+            cout << "N = " << numItem << endl;
+        run();
+        numItem++;
     }
     return 0;
 }
