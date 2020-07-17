d1ceabba3bc392fdcbb7caba6fc6f44ffb104160
Mark <s108321505@mail1.ncnu.edu.tw>
2020-07-17 17:06:49 +0800

    updated

--- main.cpp
+++ main.cpp
@@ -12,6 +12,27 @@ int numSearch = 1; // the number of search, i.e., iteration count.
 double ampI; // the amplitude of selected items.
 double ampO; // the amplitude of unselected items.
 
+class Sol {
+public:
+    double numItem = {0};
+    double numSearch = {0};
+    double ampI = {0};
+    double ampO = {0};
+    double probI = {0};
+    double probO = {0};
+
+    Sol &operator=(const Sol &t) = default;
+
+    bool operator!=(const Sol &t) const {
+        return this->ampI != t.ampI || this->ampO != t.ampO
+               || this->probO != t.probO || this->probI != t.probI
+               || this->numItem != t.numItem || this->numSearch != t.numSearch;
+    }
+};
+
+
+Sol best;
+
 void run() {
     for (int i = 0; i < numSearch; i++) {
         ampI = -ampI; // selective inversion
@@ -21,21 +42,34 @@ void run() {
         ampI = 2.0 * avg - ampI;
         ampO = 2.0 * avg - ampO;
 
-        Logger logger("../result" + to_string(int(numItem / 10000000)) + ".csv");
-        logger.writeComma(numItem);
-        logger.writeComma(ampI);
-        logger.writeComma(ampO);
-        logger.writeComma(i + 1);
-        if (abs(ampO * (numItem - numSelectedItem)) <= 0.0000000000000001)
-            logger.writeLine(1); // found
-        else
-            logger.writeLine(0); // not found
+        // update best
+        if (abs(1 - best.ampI) > abs(1 - ampI) && numItem != 4) {
+            best.ampI = ampI;
+            best.ampO = ampO;
+            best.numSearch = numSearch;
+            best.numItem = numItem;
+            best.probI = ampI * ampI;
+            best.probO = ampO * ampO;
+        }
+
+        // output
+        if (i >= numSearch - 2) {
+            Logger logger("../result" + to_string(int(numItem / 100000)) + ".csv");
+            logger.writeComma(numItem);
+            logger.writeComma(ampI);
+            logger.writeComma(ampO);
+            logger.writeComma(i + 1);
+            if (abs(ampO * (numItem - numSelectedItem)) <= 0.0000000000000001)
+                logger.writeLine(1); // found
+            else
+                logger.writeLine(0); // not found
+        }
     }
 }
 
 int main() {
     long long max = pow(2, 31);
-
+    Sol tmp;
     while (numItem < max) {
         ampI = 1.0 / sqrt(numItem);
         ampO = 1.0 / sqrt(numItem);
@@ -45,6 +79,18 @@ int main() {
             cout << "N = " << numItem << endl;
         run();
         numItem++;
+
+        // if best has changed, print best
+        if (best != tmp) {
+            Logger logger("../best.csv");
+            logger.writeComma(best.numItem);
+            logger.writeComma(best.ampI);
+            logger.writeComma(best.ampO);
+            logger.writeComma(best.probI);
+            logger.writeComma(best.probO);
+            logger.writeLine(best.numSearch + 1);
+            tmp = best;
+        }
     }
     return 0;
 }
